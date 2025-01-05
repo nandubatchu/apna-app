e77ddc722296862bbbabc9a6f5b69a59c5ebf10d
Yadunandan Batchu <nandubatchu@gmail.com>
2025-01-05 19:48:43 +0000

    test route caching

--- app/api/nostr/pool/get/route.ts
+++ app/api/nostr/pool/get/route.ts
@@ -4,10 +4,16 @@ import { SimplePool } from 'nostr-tools/pool'
 
 // import WebSocket from 'ws'
 // useWebSocketImplementation(WebSocket)
-
+export const dynamic = 'force-dynamic';
 export async function GET() {
     await new Promise((resolve) => {setTimeout(resolve, 5000)})
+    // const pool = new SimplePool()
 
+    // let relays = ["wss://relay.damus.io/"]
+
+    // let event = await pool.get(relays, {
+    //     ids: ['3fa47699cbed04c37f35aeb80e2cdccfb55078ac36d6c3d1aef267c97c086ed4'],
+    // })
     return new Response((new Date).toLocaleString(), {
         status: 200,
         headers: {
