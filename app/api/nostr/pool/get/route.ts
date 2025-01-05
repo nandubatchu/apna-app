c8293ac14c699b874d735de4e1ac52c8274dff0f
Yadunandan Batchu <nandubatchu@gmail.com>
2025-01-05 20:02:51 +0000

    test route caching

--- app/api/nostr/pool/get/route.ts
+++ app/api/nostr/pool/get/route.ts
@@ -6,18 +6,18 @@ import { SimplePool } from 'nostr-tools/pool'
 // useWebSocketImplementation(WebSocket)
 export const dynamic = 'force-dynamic';
 export async function GET() {
-    await new Promise((resolve) => {setTimeout(resolve, 5000)})
-    // const pool = new SimplePool()
+    // await new Promise((resolve) => {setTimeout(resolve, 5000)})
+    const pool = new SimplePool()
 
-    // let relays = ["wss://relay.damus.io/"]
+    let relays = ["wss://relay.damus.io/"]
 
-    // let event = await pool.get(relays, {
-    //     ids: ['3fa47699cbed04c37f35aeb80e2cdccfb55078ac36d6c3d1aef267c97c086ed4'],
-    // })
+    let event = await pool.get(relays, {
+        ids: ['3fa47699cbed04c37f35aeb80e2cdccfb55078ac36d6c3d1aef267c97c086ed4'],
+    })
     return new Response((new Date).toLocaleString(), {
         status: 200,
         headers: {
-            'Cache-Control': 'max-age=10, s-maxage=20, stale-while-revalidate=10'
+            'Cache-Control': 'max-age=60, s-maxage=120, stale-while-revalidate=9'
         },
     });
 }
