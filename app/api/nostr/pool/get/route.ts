f33b9378409526e61adaf83cc2b133d76f28fdd1
Yadunandan Batchu <nandubatchu@gmail.com>
2025-01-05 20:40:43 +0000

    enable multiple events query

--- app/api/nostr/pool/get/route.ts
+++ app/api/nostr/pool/get/route.ts
@@ -9,6 +9,7 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const noCache = Boolean(searchParams.get('noCache'))
+    const isSingleEvent = Boolean(searchParams.get('isSingleEvent'))
     const filter = searchParams.get('filter')
 
     if (!filter) {
@@ -26,13 +27,18 @@ export async function GET(request: NextRequest) {
     // let event = await pool.get(relays, {
     //     ids: ['3fa47699cbed04c37f35aeb80e2cdccfb55078ac36d6c3d1aef267c97c086ed4'],
     // })
-    let event = await pool.get(relays, filterObj)
+    let result
+    if (isSingleEvent) {
+        result = await pool.get(relays, filterObj)
+    } else {
+        result = await pool.querySync(relays, filterObj)
+    }
 
     const headers: any = {}
     if (!noCache) {
         headers['Cache-Control'] = 'public, s-maxage=60, stale-while-revalidate=60'
     }
-    return new Response(JSON.stringify(event), {
+    return new Response(JSON.stringify(result), {
         status: 200,
         headers,
     });
