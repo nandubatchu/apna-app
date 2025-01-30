e1df48e56878b48c2973c0700f7ddbdc243fe879
Yadunandan Batchu <nandubatchu@gmail.com>
2025-01-30 20:36:22 +0000

    update constants for public posting

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
 // Nostr Note IDs
-export const FEEDBACK_ROOT_NOTE_ID = "9e3978eb3a0fd22ef31b9908586faac17ec2854313be30927494a05440010de0";
-export const APPS_ROOT_NOTE_ID = "3fa47699cbed04c37f35aeb80e2cdccfb55078ac36d6c3d1aef267c97c086ed4";
+export const FEEDBACK_ROOT_NOTE_ID = "915148f7647fe9ef2f37858e9038140b0313d2fab6419807566adf548339f6a7";
+export const APPS_ROOT_NOTE_ID = "6b88cbff9450a49567c2a6ea2158edfe2609e634e83d154e7c70656ce0f34b8b";
